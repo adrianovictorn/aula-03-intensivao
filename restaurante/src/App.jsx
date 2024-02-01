65825306fd7e2c08e4804a510afd79f22bef4dbf
Adriano Victor N. Ribeiro <115894997+adrianovictorn@users.noreply.github.com>
2024-01-31 21:44:36 -0300

    atualizei

--- restaurante/src/App.jsx
+++ restaurante/src/App.jsx
@@ -1,16 +1,20 @@
+import { useState } from "react";
 import Hashtaurante from "./assets/hashtaurante.webp";
 import './App.css';
 import { Navegacao } from "./Navegacao";
 import { ItemCardapio } from "./itemCardapio";
 import { pratosPrincipais,sobremesas,bebidas } from "./cardapio";
+
+
 export function App(){
-  
+  const paginasMenu = [pratosPrincipais,sobremesas,bebidas];
+  const [paginaSelecionada, atualizarPaginaSelecionada] = useState(0);
   
   return <>
         <img src={Hashtaurante} alt="" className="capa"/>
-        <Navegacao />
+        <Navegacao atualizarPaginaSelecionada={atualizarPaginaSelecionada}/>
         <div className="menu">
-         {pratosPrincipais.map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem}/>)}
+         {paginasMenu[paginaSelecionada].map(item => <ItemCardapio nome={item.nome} descricao={item.descricao} preco={item.preco} imagem={item.imagem}/>)}
         </div>
   </> 
 
