441ecd4c3e614ba9ed5daa762b64eb1f182d9986
Adriano Victor N. Ribeiro <115894997+adrianovictorn@users.noreply.github.com>
2024-01-31 21:20:32 -0300

    atualizei

--- restaurante/src/App.jsx
+++ restaurante/src/App.jsx
@@ -2,13 +2,16 @@ import Hashtaurante from "./assets/hashtaurante.webp";
 import './App.css';
 import { Navegacao } from "./Navegacao";
 import { ItemCardapio } from "./itemCardapio";
+import { pratosPrincipais,sobremesas,bebidas } from "./cardapio";
 export function App(){
   
   
   return <>
         <img src={Hashtaurante} alt="" className="capa"/>
         <Navegacao />
-        <ItemCardapio/>
+        <div className="menu">
+         {}
+        </div>
   </> 
 
 }
